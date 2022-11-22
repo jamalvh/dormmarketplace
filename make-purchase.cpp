7d707b7e2b42959cc87dff3ca134ae1b79235a63
jamalvh <113135025+jamalvh@users.noreply.github.com>
2022-11-22 01:47:56 -0500

    Update make-purchase.cpp

--- make-purchase.cpp
+++ make-purchase.cpp
@@ -57,7 +57,7 @@ void findProduct(string& id, string& price, string& item) {
     
     //bool foundProduct = false;
     
-    ifstream marketListings("marketListings");
+    ifstream marketListings("marketListings.txt");
     
     while (marketListings >> id) {
         marketListings >> price;
@@ -133,7 +133,94 @@ void completePurchase(string id, string price, string item, string& email) {
 
 void removeListing(string id, string price, string item, string& email) {
     
-    ifstream input_file("marketListings.txt");
+    //UPDATE MARKET
+    vector<string> idPriceItemCopy = {};
+    
+    string idTemp = "";
+    string priceTemp = "";
+    string itemTemp = "";
+    string blank = "";
+    
+    ifstream downloadMarket("marketListings.txt");
+    
+    while (downloadMarket >> idTemp) {
+        
+        downloadMarket >> priceTemp;
+        getline(downloadMarket, blank);
+        getline(downloadMarket, itemTemp);
+        
+        if (idTemp != id) {
+        
+            idPriceItemCopy.push_back(idTemp);
+            idPriceItemCopy.push_back(priceTemp);
+            idPriceItemCopy.push_back(itemTemp);
+
+        }
+    }
+    
+    cout << endl;
+    
+    for (int k = 0; k < idPriceItemCopy.size(); k++) {
+        
+        cout << idPriceItemCopy[k];
+        if (k % 2 == 0) {
+            cout << ", ";
+        } else {
+            cout << " ";
+        }
+        
+    }
+    
+    cout << endl;
+    
+    downloadMarket.close();
+
+    
+    ofstream uploadNewMarket("marketListings.txt");
+    
+    for (int i = 0; i < idPriceItemCopy.size(); i++) {
+        
+        idTemp = idPriceItemCopy[i];
+        i++;
+        priceTemp = idPriceItemCopy[i];
+        i++;
+        itemTemp = idPriceItemCopy[i];
+        
+        uploadNewMarket << idTemp << " " << priceTemp << endl
+                     << itemTemp << endl;
+    }
+        
+    uploadNewMarket.close();
     
     
+    
+    //UPDATE PERSONAL
+    vector<string> pIdPriceItemCopy = {};
+    
+    string pIdTemp;
+    string pPriceTemp;
+    string pItemTemp;
+    string pBlank;
+    string passwordTemp;
+    
+    ifstream downloadPersonal("marketListings.txt");
+    
+    downloadPersonal >> passwordTemp;
+    getline(downloadPersonal, pBlank);
+    
+    while (downloadPersonal >> pIdTemp >> pPriceTemp) {
+    
+        getline(downloadPersonal, pBlank);
+        getline(downloadPersonal, pItemTemp);
+        
+        if (pIdTemp != id) {
+        
+            pIdPriceItemCopy.push_back(pIdTemp);
+            pIdPriceItemCopy.push_back(pPriceTemp);
+            pIdPriceItemCopy.push_back(pItemTemp);
+
+        }
+    }
+    
+    downloadPersonal.close();
 }
